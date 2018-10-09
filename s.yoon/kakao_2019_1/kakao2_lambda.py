42c5117d37f289a0c7083136ca0306a2e71849b5
s.yoon <senel.study@gmail.com>
2018-10-09 18:24:19 +0900

    2019_2_lambda

---
+++ s.yoon/kakao_2019_1/kakao2_lambda.py
@@ -0,0 +1,20 @@
+def failure(n, stages):
+    total = len(stages)
+    fail = []
+    for i in range(1, n+1):
+        challenger = stages.count(i)
+        fail.append([challenger/total, i])
+        total-=challenger
+    s = sorted(fail, key = lambda x: (-x[0], x[1]))
+    n_fail = [x[1] for x in s]
+    
+    return n_fail
+        
+
+n_list = [5,4]
+s_list = [[2,1,2,6,2,4,3,3], [4,4,4,4,4]]
+
+result = [ failure(s, c) for s,c  in zip(n_list,s_list) ]
+
+# result = failure(5,[2,1,2,6,2,4,3,3])
+print(result)
