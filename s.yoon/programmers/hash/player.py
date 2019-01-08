ed29adc2dd843649bb9a63f6572475e7e21cb2ea
s.yoon <s.yoon@micro-wave.net>
2019-01-08 19:00:49 +0900

    완주하지 못한 선수

---
+++ s.yoon/programmers/hash/player.py
@@ -0,0 +1,24 @@
+participant = ["marina", "josipa", "nikola", "vinko", "filipa", "filipa"]	
+completion = ["josipa", "filipa", "marina", "filipa", "nikola"]
+
+def solution(participant, completion):
+    # 동명이인검사
+    answer = ''
+    isDuplicated = ( len(participant) == len(set(participant)) )
+
+    if(isDuplicated):
+        player = set(participant) - set(completion)
+        answer = list(player)[0]
+        return answer
+    else:
+        participant.sort()
+        completion.sort()
+        for i, s in enumerate(completion):
+            if(participant[i] != s):
+                answer = participant[i]
+                return answer
+        answer = participant[-1]
+        return answer
+
+
+print(solution(participant, completion))
